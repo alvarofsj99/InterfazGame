e3d1abecd57dce9624a554fde0f32f75b5527120
informaticaesne <informatica@esne.es>
2023-10-04 17:52:18 +0200

    Pruebas Ventanas 2

--- Assets/Scripts/ventanaUI.cs
+++ Assets/Scripts/ventanaUI.cs
@@ -9,6 +9,15 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 
     [SerializeField] PointerEventData.InputButton inputInteract;
 
+    RectTransform rectTransform;
+
+    public float borderSnapSize = 20;
+
+    private void Awake()
+    {
+        rectTransform = (RectTransform)transform;
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         if(eventData.button == inputInteract)
@@ -25,15 +34,29 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void TrapToScreen()
     {
-        
-    }
+        Vector3 diffMin = rectTransform.position + (Vector3)rectTransform.rect.position;
+        Vector3 diffMax = (Vector3)Camera.main.pixelRect.size - rectTransform.position + (Vector3)rectTransform.rect.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(diffMin.x < borderSnapSize)
+        {
+            rectTransform.position -= new Vector3(diffMin.x, 0, 0);
+        }
+
+        if(diffMin.y < borderSnapSize)
+        {
+            rectTransform.position -= new Vector3(0, diffMin.y, 0);
+        }
+
+        if (diffMax.x < borderSnapSize)
+        {
+            rectTransform.position += new Vector3(diffMax.x, 0, 0);
+        }
+
+        if (diffMax.y < borderSnapSize)
+        {
+            rectTransform.position += new Vector3(0, diffMax.y, 0);
+        }
     }
 }
