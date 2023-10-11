f005ccb951f092d1d16e69ef3bb509d6459604d7
informaticaesne <hectordlpg2415@gmail.com>
2023-10-11 17:13:47 +0200

    Pruebas Ventana
    Añadidas las ventanas e interacción con las mismas.

--- Assets/Scripts/ventanaUI.cs
+++ Assets/Scripts/ventanaUI.cs
@@ -11,18 +11,26 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 
     RectTransform rectTransform;
 
-    public float borderSnapSize = 20;
+    RectTransform rectTransformPC;
+
+    public float borderSnapSize = 10;
+
+    static HashSet<ventanaUI> allWindows = new HashSet<ventanaUI>();
 
     private void Awake()
     {
         rectTransform = (RectTransform)transform;
+
+        allWindows.Add(this);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         if(eventData.button == inputInteract)
         {
-            transform.localPosition = Input.mousePosition - mouseStartPos;
+            transform.position = Input.mousePosition - mouseStartPos;
+            TrapToScreen();
+            SnapOtherWindows();
         }
     }
 
@@ -30,7 +38,7 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
     {
         if (eventData.button == inputInteract)
         {
-            mouseStartPos = Input.mousePosition - transform.localPosition;
+            mouseStartPos = Input.mousePosition - transform.position;
         }
     }
 
@@ -39,7 +47,7 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
         Vector3 diffMin = rectTransform.position + (Vector3)rectTransform.rect.position;
         Vector3 diffMax = (Vector3)Camera.main.pixelRect.size - rectTransform.position + (Vector3)rectTransform.rect.position;
 
-        if(diffMin.x < borderSnapSize)
+        if (diffMin.x < borderSnapSize)
         {
             rectTransform.position -= new Vector3(diffMin.x, 0, 0);
         }
@@ -57,6 +65,38 @@ public class ventanaUI : MonoBehaviour, IDragHandler, IPointerDownHandler
         if (diffMax.y < borderSnapSize)
         {
             rectTransform.position += new Vector3(0, diffMax.y, 0);
+        }
+    }
+
+    private void SnapOtherWindows()
+    {
+        foreach(ventanaUI ven in allWindows)
+        {
+            if (ven == this)
+            {
+                continue;
+            }
+            if (ven.gameObject.activeInHierarchy)
+            {
+                Vector3 DiffMin = ven.rectTransform.position - (Vector3) (ven.rectTransform.rect.position + rectTransform.rect.position) - rectTransform.position;    
+                Vector3 DIffMax = ven.rectTransform.position + (Vector3)(ven.rectTransform.rect.position + rectTransform.rect.position) - rectTransform.position;
+                if (Mathf.Abs(DiffMin.x) < borderSnapSize)
+                {
+                    rectTransform.position += new Vector3(DiffMin.x, 0, 0);
+                }
+                if (Mathf.Abs(DiffMin.y) < borderSnapSize)
+                {
+                    rectTransform.position += new Vector3(0, DiffMin.y, 0);
+                }
+                if (Mathf.Abs(DIffMax.x) < borderSnapSize)
+                {
+                    rectTransform.position += new Vector3(DIffMax.x, 0, 0);
+                }
+                if (Mathf.Abs(DIffMax.y) < borderSnapSize)
+                {
+                    rectTransform.position += new Vector3(0, DIffMax.y, 0);
+                }
+            }
         }
     }
 }
